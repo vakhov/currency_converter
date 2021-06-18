0c7d022660cd1544bd94fd46a16082ad4dc810fe
Alex Vakhov <alex.vakhov@gmail.com>
2021-06-18 14:36:08 +0500

    [change] small changes

--- currency_converter/app/services/api/converter.py
+++ currency_converter/app/services/api/converter.py
@@ -1,7 +1,9 @@
 """"""
+import asyncio
 from decimal import Decimal
 
-from redis import Redis
+import aioredis
+from aioredis import Redis
 
 from app.helpers.klasses import Singleton, Environ
 
@@ -12,16 +14,22 @@ class ConverterApi(metaclass=Singleton):
     """API конвертера валют"""
 
     def __init__(self) -> None:
-        self._redis = Redis('redis', password=env['REDIS_DEFAULT_PASS'])
+        self._redis: Redis = asyncio.run(self.connect())
 
-    def flush(self) -> None:
+    async def connect(self) -> Redis:
+        return await aioredis.create_redis_pool(
+            address=env['REDIS_URL'],
+            password=env['REDIS_DEFAULT_PASS']
+        )
+
+    async def flush(self) -> None:
         """Удаляет все ключи из текущей базы"""
-        self._redis.flushdb()
+        await self._redis.flushdb()
 
-    def get(self, currenci: str) -> Decimal:
+    async def get(self, currency: str) -> Decimal:
         """Получение курса"""
         raise NotImplementedError()
 
-    def update(self, data):
+    async def update(self, data):
         """Update данных"""
         raise NotImplementedError()
--- currency_converter/main.py
+++ currency_converter/main.py
@@ -1,13 +1,32 @@
+import aioredis
 from aiohttp.web import run_app
 from aiohttp.web_app import Application
 
 from app.helpers.klasses import Environ
 from app.routes import routes
+from app.services.api.converter import ConverterApi
+
+
+async def connect_to_redis(url: str):
+    redis = await aioredis.create_redis_pool(url)
+    return redis
+
+
+class RedisConnetion:
+
+    def __init__(self, app: Application) -> None:
+        self._app = app
+
+    async def on_startup(self):
+        setattr(self._app, 'client', ConverterApi())
+
+    async def cleanup(self):
+        pass
+
 
 env = Environ()
 
 app = Application()
-
 app.add_routes(routes=routes)
 
 if __name__ == '__main__':
